4ca02cd3ec9def669a727ff7d6d7074d59fc5a69
Badal Chauhan <badalch10@gmail.com>
2023-07-18 18:56:57 +0530

    changes

--- src/components/Certificate.js
+++ src/components/Certificate.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Cert_1 from './images/cert1.jpg';
 import Cert_2 from './images/cert2.jpg';
-import Cert_3 from './images/cert3.jpg';
 
 function Certificate() {
   return (
@@ -10,7 +9,6 @@ function Certificate() {
       <div>
         <img src={Cert_1} alt="Certificate 1" style={{ margin: '10px', width: '350px' }}/>
         <img src={Cert_2} alt="Certificate 2" style={{ margin: '10px', width: '350px' }}/>
-        <img src={Cert_3} alt="Certificate 3" style={{ margin: '10px', width: '350px' }}/>
       </div>
     </section>
   );
